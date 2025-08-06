ad2b5dd991affb3307ea7f1c0ae21a9122934c5e
Subrata Biswas <149804850+subratabisw@users.noreply.github.com>
2025-08-06 21:12:56 +0600

    Add files via upload

---
+++ len_wid.py
@@ -0,0 +1,6 @@
+a = float(input("Enter the length of the rectangle: "))
+b = float(input("Enter the width of the rectangle: "))
+area = a * b
+print("The area of the rectangle is:", area)
+perimeter = 2 * (a + b)
+print(("The perimeter of the rectangle is:",perimeter))
