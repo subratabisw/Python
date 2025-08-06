26213375513517a32c8998109dbbabb05779b33d
Subrata Biswas <149804850+subratabisw@users.noreply.github.com>
2025-08-06 21:14:32 +0600

    Add files via upload

---
+++ rad.py
@@ -0,0 +1,8 @@
+import math
+a = float(input("Enter the radius of the circle:"))
+diameter = 2 * a
+circumference = 2 * math.pi * a
+area = math.pi * (a ** 2)
+print("Diameter of the circle =", diameter)
+print("Circumference of the circle =", circumference)
+print("Area of the circle =", area)
